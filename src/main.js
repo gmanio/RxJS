f3ad54e31a6be4a5222f76f7646675a33147c391
gman.park <im@gman.io>
2017-01-20 12:18:41 +0900

    [TASK] product search API rx test

--- src/main.js
+++ src/main.js
@@ -6,24 +6,45 @@
 var Rx = require("rxjs");
 var Main = (function () {
     function Main() {
+        this.searchKeyword = "nike";
         this.attachEvent();
+        //Init load datas.
+        this.requestData();
     }
     Main.prototype.attachEvent = function () {
+        var _this = this;
         var input = document.querySelector('.inp');
         Rx.Observable.fromEvent(input, 'keyup')
-            .debounceTime(1000);
-        this.requestData();
+            .debounceTime(1000)
+            .map(function (e) {
+            return e.target.value;
+        })
+            .distinctUntilChanged()
+            .filter(function (text) {
+            // searchText null validation.
+            return text != null;
+        })
+            .subscribe(function (text) {
+            _this.searchKeyword = text;
+            if (text != null) {
+                _this.requestData();
+            }
+        }, function (err) {
+            console.log(err);
+        });
     };
     Main.prototype.requestData = function () {
+        console.log(this.searchKeyword);
         Rx.Observable
             .ajax({
-            url: 'http://apis.skplanetx.com/11st/v2/common/products?appKey=' + Main.App_key + '&searchKeyword=nike&sortCode=A',
+            url: 'http://apis.skplanetx.com/11st/v2/common/products?appKey=' + Main.App_key + '&searchKeyword=' + this.searchKeyword + '&sortCode=A',
             crossDomain: true,
             headers: {
                 "Content-type": "application/json",
                 "Accept": "application/json"
             }
         })
+            .retry(3)
             .map(function (e) { return e.response; })
             .map(function (e) {
             if (e.ProductSearchResponse) {
@@ -31,11 +52,22 @@ var Main = (function () {
             }
         })
             .subscribe(function (res) {
-            console.log(res);
+            var wrapper = document.querySelector('.item_list');
+            wrapper.innerHTML = "";
+            for (var i = 0; i < res.length; i++) {
+                // Html5 <template> import
+                var template = document.querySelector('#tmpl_item');
+                var elImg = template.content.querySelector('img');
+                elImg.src = res[i].ProductImage;
+                var clone = document.importNode(template.content, true);
+                wrapper.appendChild(clone);
+            }
+        }, function (err) {
+            console.log(err);
         });
     };
     return Main;
 }());
-// for SK weather API.
+// for SK OpenAPI devKey.
 Main.App_key = 'f0c12da8-2ef4-3e24-8815-e2f347909a80';
 var App = new Main();
--- src/main.ts
+++ src/main.ts
@@ -5,41 +5,86 @@
 import * as Rx from 'rxjs';
 
 class Main {
-    // for SK weather API.
+    // for SK OpenAPI devKey.
     private static App_key = 'f0c12da8-2ef4-3e24-8815-e2f347909a80';
+    private searchKeyword = "nike";
 
     constructor() {
         this.attachEvent();
-    }
 
-    attachEvent() {
-        let input = document.querySelector('.inp');
-
-        Rx.Observable.fromEvent(input, 'keyup')
-            .debounceTime(1000);
-
+        //Init load datas.
         this.requestData();
     }
 
+    attachEvent() {
+        let input = <Element>document.querySelector('.inp');
+
+        Rx.Observable.fromEvent(input, 'keyup')
+            .debounceTime(1000)
+            .map(function (e) {
+                return e.target.value;
+            })
+            .distinctUntilChanged()
+            // .scan(function (prev, current) {
+            //     if (prev == null) {
+            //         return null;
+            //     }
+            //     return current;
+            // }, null)
+            .filter(function (text) {
+                // searchText null validation.
+                return text != null
+            })
+            .subscribe(
+                text => {
+                    this.searchKeyword = text;
+                    if (text != null) {
+                        this.requestData();
+                    }
+                },
+                (err) => {
+                    console.log(err);
+                }
+            );
+    }
+
     requestData() {
+        console.log(this.searchKeyword);
         Rx.Observable
             .ajax({
-                url: 'http://apis.skplanetx.com/11st/v2/common/products?appKey=' + Main.App_key + '&searchKeyword=nike&sortCode=A',
+                url: 'http://apis.skplanetx.com/11st/v2/common/products?appKey=' + Main.App_key + '&searchKeyword=' + this.searchKeyword + '&sortCode=A',
                 crossDomain: true,
                 headers: {
                     "Content-type": "application/json",
                     "Accept": "application/json"
                 }
             })
+            .retry(3)
             .map(e => e.response)
             .map(e => {
-                if(e.ProductSearchResponse){
+                if (e.ProductSearchResponse) {
                     return e.ProductSearchResponse.Products.Product
                 }
             })
             .subscribe(
                 (res) => {
-                    console.log(res);
+                    let wrapper = <Element>document.querySelector('.item_list');
+
+                    wrapper.innerHTML = "";
+
+                    for (var i = 0; i < res.length; i++) {
+
+                        // Html5 <template> import
+                        let template = <Element>document.querySelector('#tmpl_item');
+                        let elImg = template.content.querySelector('img');
+                        elImg.src = res[i].ProductImage;
+
+                        let clone = document.importNode(template.content, true);
+                        wrapper.appendChild(clone);
+                    }
+                },
+                (err) => {
+                    console.log(err);
                 }
             );
     }
